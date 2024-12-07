2e31bf4deb0738b8fed9478ad04cd0eec688b8d8
Your Name <you@example.com>
2024-12-07 15:11:35 +0200

    navbar

--- Assets/js/all.js
+++ Assets/js/all.js
@@ -3,10 +3,17 @@
 
 window.onscroll=()=>{
     if(window.scrollY>400){
-        document.querySelector(".nav").style.backgroundColor="red";
+        document.querySelector(".nav").classList.add("nav-scroll");
+        document.querySelector(".brand-text").style.color="var(--primary)";
+        document.querySelector(".brand-logo").style.color="var(--primary)";
+        document.querySelector(".nav").style.padding="0px ";
 
     }
     else{
-        document.querySelector(".nav").style.backgroundColor="transparent";
+        document.querySelector(".brand-text").style.color="var(--white)";
+        document.querySelector(".brand-logo").style.color="var(--white)";
+        document.querySelector(".nav").classList.remove("nav-scroll");
+        document.querySelector(".nav").style.padding="10px 0px ";
+        
     }
 }
--- Assets/js/indexjs/swipe-index.js
+++ Assets/js/indexjs/swipe-index.js
@@ -13,4 +13,22 @@ const swiper = new Swiper('.swiper', {
       slidesPerView: 3,
       speed: 2000,
       slidesPerGroup: 3,
+      breakpoints: {
+        // when window width is >= 320px
+        500: {
+          slidesPerView: 1,
+          slidesPerGroup: 1,
+        },
+        // when window width is >= 480px
+        720: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+          slidesPerGroup: 2,
+        },
+        // when window width is >= 640px
+        900: {
+          slidesPerView: 3,
+          spaceBetween: 20
+        }
+      }
   });
